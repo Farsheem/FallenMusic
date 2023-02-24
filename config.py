baf93835c5768d12d2e05fbe632bcc261641f658
Farsheem <118446335+Farsheem@users.noreply.github.com>
2023-02-24 19:36:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("90", "90"))
 
 OWNER_ID = int(getenv("1083660482"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://daily.jstor.org/wp-content/uploads/2023/01/good_times_with_bad_music_1050x700.jpg")
+START_IMG = getenv("START_IMG", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZPoi5hp0YLqI5i6zY2HGXI5Bezabs2Xis6A&usqp=CAU")
 
-SESSION = getenv("BQF5vTwAt6oKGDPHm4jTCF4iG0ZpqNKLb45n3vFfLRrlL15QPnujRUlykreUvpCYmIg70vkr1JZgfmYE5wAU8LOv5pNGLnyNolR-NrpATOykE6daKNI34WbVJ00glpVzCYOFMxZKNtnV4cYDglThad8xW367cCnCl9QCI-EdVGYxm5zUx4jsK0PKaihu0VyjKPZYO6jlE2njSfh9QzYxr1pyVSLFfGAi8fI_WdcvCqettS6veRkTp-llfGgDyexLdzUFN0SHKkzRjx06qGFOcxlGVcSNvQG-tHRGaxQ9EPDPAB6GG4z48ZfVrAxfOEYANtatdyyBg4Y_sAS4wvdfgQ2KG9EsNQAAAAB_nhrNAA", None)
+SESSION = getenv("BQAjvNwGPR0k2fZ6EAQ_fWQeZmfnfjHA32gFk84pZ54wXSG_WjIgrhhP4wVW6Ifzqn2ZtCuoY6x4cW1EL2AYYFrG6o1w4YycmasifC5gYFHollX02Lslrxnq2RTlWljvICPAz86Xs_bJ0EokF48TAb-FLwTg788ovCUgasJonu-sPY2-_o0FItPyvhiI-naPgCVODCiiHA6XcR_qT3ytKL1mcVR6YB-fyTT_NfQ1irmJ-LVKyyLLRPFTN8vAlZ1EF9zmaE7j43Oh7vPxdV6uTyJzf6TaBPwVkqyabXgKDYRZmbdXJkf18gGg-IRbAK5h9y_NTvPo-_exQsv5QE2wNlinf54azQA", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/the_squaaad")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_faiz")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1083660482").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
